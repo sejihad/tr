8cee68875b4eb51f128deb4b4c8e9e7c987bccba
unknown <ijihad639@gmail.com>
2025-04-18 10:17:04 +0600

    tr

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://tr-api-f4ge.onrender.com");
 
 function App() {
   const [isCalling, setIsCalling] = useState(false);
